08913e3b9f7877ca713dd109601deaa1652d780f
Eric <eah0592@gmail.com>
2020-12-11 18:04:19 -0600

    Fix 500 response when missing request params

--- goty-server/src/main/java/com/aleinin/goty/game/GameSearchRequest.kt
+++ goty-server/src/main/java/com/aleinin/goty/game/GameSearchRequest.kt
@@ -1,7 +1,9 @@
 package com.aleinin.goty.game
 
+import org.springframework.web.bind.annotation.RequestParam
+
 data class GameSearchRequest(
-        val name: String,
-        val limit: Int = 5,
-        val year: Int?,
-        val mainGame: Boolean = false)
+        @RequestParam val name: String,
+        @RequestParam(defaultValue = "5") val limit: Int = 5,
+        @RequestParam(required = false) val year: Int?,
+        @RequestParam(defaultValue = "false") val mainGame: Boolean = false)

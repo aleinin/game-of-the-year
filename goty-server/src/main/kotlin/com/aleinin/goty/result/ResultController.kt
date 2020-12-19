160b9aaa17bd6ba4f30dd0b84d5207ea9261b85a
Eric <eah0592@gmail.com>
2020-12-19 11:29:58 -0600

    #10 Add cross origin support for result controller

--- goty-server/src/main/kotlin/com/aleinin/goty/result/ResultController.kt
+++ goty-server/src/main/kotlin/com/aleinin/goty/result/ResultController.kt
@@ -1,9 +1,11 @@
 package com.aleinin.goty.result
 
 import com.aleinin.goty.submit.SubmissionRepository
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 class ResultController(
     private val submissionRepository: SubmissionRepository,

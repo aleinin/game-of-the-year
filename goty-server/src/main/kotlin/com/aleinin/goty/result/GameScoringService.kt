bb8b2cdf6e4596b0b6ce712c0fec55ca133b6034
Eric <eah0592@gmail.com>
2020-12-23 10:40:04 -0600

    #10 Return all results from /results endpoint

---
+++ goty-server/src/main/kotlin/com/aleinin/goty/result/GameScoringService.kt
@@ -0,0 +1,33 @@
+package com.aleinin.goty.result
+
+import com.aleinin.goty.submit.RankedGameSubmission
+import org.springframework.stereotype.Component
+
+@Component
+class GameScoringService(private val pointsService: PointsService) {
+
+    fun score(games: List<RankedGameSubmission>) =
+        games
+            .groupingBy { it.id }
+            .fold(
+                { id, first -> ScoredGameResult(id = id, title = first.title) },
+                { _, rankedGameResult, rankedGameSubmission ->
+                    rankedGameResult.copy(
+                        points = rankedGameResult.points + pointsService.calculatePoints(rankedGameSubmission.rank),
+                        votes = rankedGameResult.votes + 1,
+                    )
+                },
+            )
+            .map { it.value }
+            .groupBy { ScoreKey(it.votes, it.points) }
+            .toSortedMap()
+            .asSequence()
+            .flatMapIndexed { rank, (_, group) -> group.map { it.copy(rank = rank) } }
+            .toList()
+
+    private data class ScoreKey(val votes: Int, val points: Int) : Comparable<ScoreKey> {
+
+        override fun compareTo(other: ScoreKey) =
+            if (other.votes == votes) other.points - points else other.votes - votes
+    }
+}
--- goty-server/src/main/kotlin/com/aleinin/goty/result/ScoredGameResult.kt
+++ goty-server/src/main/kotlin/com/aleinin/goty/result/ScoredGameResult.kt
@@ -1,8 +1,8 @@
 package com.aleinin.goty.result
 
 data class ScoredGameResult(
-    val id: String = "",
-    val title: String = "",
+    val id: String,
+    val title: String,
     val points: Int = 0,
     val votes: Int = 0,
     val rank: Int = 0,

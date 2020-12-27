37c3cf3bbfe5f773ee8204e052981e5cefbb5301
Andrew <aleinin@siue.edu>
2020-12-27 11:18:16 -0600

    remove unnecesary checks

--- goty-client/src/app/results/results-table/results-table-style.pipe.ts
+++ goty-client/src/app/results/results-table/results-table-style.pipe.ts
@@ -6,21 +6,15 @@ const second = 1
 const third = 2
 const tenth = 9
 
-const isInGOTYTop10ButNotTop3 = (rank: number, styleType: StyleType) =>
-  (styleType === 'goty' && rank > third && rank <= tenth)
-
-const isMissingRankAndOrStyleType = (rank: number, styleType: StyleType) =>
-  rank == null || styleType === 'none'
+const isEliminated = (rank: number, styleType: StyleType) =>
+  (styleType === 'single' && rank > first) || (styleType === 'goty' && rank > tenth)
 
 @Pipe({
   name: 'resultsTableStylePipe'
 })
 export class ResultsTableStylePipe implements PipeTransform {
   transform(rank: number, styleType: StyleType): string {
-    if (isMissingRankAndOrStyleType(rank, styleType) || isInGOTYTop10ButNotTop3(rank, styleType)) {
-      return ''
-    }
-    if ((styleType === 'single' && rank > first) || (styleType === 'goty' && rank > tenth)) {
+    if (isEliminated(rank, styleType)) {
       return 'ranked-eliminated'
     }
     switch (rank) {

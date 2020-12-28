ae98f0d5e6c9975ceab90b6899e408b2c754bb8d
Andrew <aleinin@siue.edu>
2020-12-27 21:35:40 -0600

    Simplify Guard. UI => SubmissionFlow, remove final traces of 'form'

--- goty-client/src/app/app-routing.module.ts
+++ goty-client/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 import {StartPageComponent} from './start-page/start-page.component'
 import {EndPageComponent} from './end-page/end-page.component'
-import {EndGuard, FormGuard} from '../api/ui/ui.guard'
 import {RecoveryComponent} from './recovery/recovery.component'
 import {ResultsComponent} from './results/results.component'
 import {ResultsSubmissionComponent} from './results/results-submission/results-submission.component'
 import {ResultsSummaryComponent} from './results/results-summary/results-summary.component'
 import {SubmissionComponent} from './submission/submission.component'
+import {SubmissionFlowGuard} from '../api/submission-flow/submission-flow.guard'
+import {SubmissionStep} from '../api/submission-flow/submission-flow.store'
 
 const routes: Routes = [
   {
@@ -15,14 +16,16 @@ const routes: Routes = [
     component: StartPageComponent
   },
   {
-    path: 'form',
+    path: 'submission',
     component: SubmissionComponent,
-    canActivate: [FormGuard]
+    canActivate: [SubmissionFlowGuard],
+    data: { attemptingToAccess: SubmissionStep.Submission }
   },
   {
     path: 'end',
     component: EndPageComponent,
-    canActivate: [EndGuard]
+    canActivate: [SubmissionFlowGuard],
+    data: { attemptingToAccess: SubmissionStep.End }
   },
   {
     path: 'recovery',

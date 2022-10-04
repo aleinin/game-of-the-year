00248bd56727b796081e01c870a99706d8e71039
Eric Haag <eah0592@gmail.com>
2022-10-04 18:50:37 -0500

    Configure the Gradle Enterprise plugin to always publish a Build Scan

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # game-of-the-year
+
+By developing this project you implicitly agree to the Gradle Terms of Use at https://gradle.com/terms-of-service.
--- goty-server/settings.gradle.kts
+++ goty-server/settings.gradle.kts
@@ -1,1 +1,16 @@
+plugins {
+    id("com.gradle.enterprise") version "3.11.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.1"
+}
+
 rootProject.name = "goty-server"
+
+gradleEnterprise {
+    buildScan {
+        publishAlways()
+
+        // By developing this project you implicitly agree to the Gradle Terms of Use.
+        termsOfServiceAgree = "yes"
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    }
+}

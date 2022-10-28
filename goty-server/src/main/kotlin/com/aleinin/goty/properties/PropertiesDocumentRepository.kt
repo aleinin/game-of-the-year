9971fd5f01cfbe7b041a598aa18a0e1932888837
aleinin <aleininger.cs@gmail.com>
2022-10-27 21:27:07 -0500

    remove repository

--- goty-server/src/main/kotlin/com/aleinin/goty/properties/PropertiesDocumentRepository.kt
+++ goty-server/src/main/kotlin/com/aleinin/goty/properties/PropertiesDocumentRepository.kt
@@ -1,7 +1,5 @@
 package com.aleinin.goty.properties
 
 import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface PropertiesDocumentRepository : MongoRepository<PropertiesDocument, String>
